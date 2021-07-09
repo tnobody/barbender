a3a383451239cc98f9c37bf303d5ce25b79129c0
Tim Keiner <keiner.tim@gmail.com>
2021-07-10 01:15:57 +0200

    add icons; complete manifest

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,16 @@ export default defineConfig({
   plugins: [
     VitePWA({
       manifest: {
-        orientation: "portrait",
         name: "BarBender",
+        orientation: "portrait",
+        background_color: "#000000",
+        theme_color: "#447857",
         description: "Calculates the plates for a barbell",
+        icons: [36, 48, 72, 96, 144, 192, 512].map((s) => ({
+          src: `/public/icons/android/launcher-icon-${s}x${s}.png`,
+          type: "image/png",
+          sizes: `${s}x${s}`,
+        })),
       },
     }),
   ],
