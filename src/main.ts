631065a0cc29a1f0b022fedd6689f4f042cc334e
Tim Keiner <keiner.tim@gmail.com>
2021-07-12 11:45:28 +0200

    register servier worker?

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { dispatch, workingWeightChanged } from "./state";
+// @ts-ignore
+import { registerSW } from 'virtual:pwa-register'
 import "./style.css";
 
 const fix100vh = () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 };
+
+registerSW({})
 
 window.addEventListener("resize", fix100vh);
 fix100vh();
--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,18 @@ export default defineConfig({
         background_color: "#000000",
         theme_color: "#447857",
         description: "Calculates the plates for a barbell",
-        icons: [36, 48, 72, 96, 144, 192, 512].map((s) => ({
-          src: `/icons/android/launcher-icon-${s}x${s}.png`,
-          type: "image/png",
-          sizes: `${s}x${s}`,
-        })),
+        icons: [
+          ...[36, 48, 72, 96, 144, 192].map((s) => ({
+            src: `/icons/android/launcher-icon-${s}x${s}.png`,
+            type: "image/png",
+            sizes: `${s}x${s}`,
+          })),
+          {
+              src:"/icons/android/splash-icon-512x512.png",
+              type: "image/png",
+              sizes: "512x512"
+          }
+        ],
       },
     }),
   ],
