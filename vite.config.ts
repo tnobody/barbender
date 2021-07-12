5bdaef1a313d7a3b0e51c6b50962abb7ea6730fe
Tim Keiner <keiner.tim@gmail.com>
2021-07-12 11:23:12 +0200

    custom manifest config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,21 @@ import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
-    VitePWA(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {},
+      manifest: {
+        name: "BarBender",
+        orientation: "portrait",
+        background_color: "#000000",
+        theme_color: "#447857",
+        description: "Calculates the plates for a barbell",
+        icons: [36, 48, 72, 96, 144, 192, 512].map((s) => ({
+          src: `/public/icons/android/launcher-icon-${s}x${s}.png`,
+          type: "image/png",
+          sizes: `${s}x${s}`,
+        })),
+      },
+    }),
   ],
 });
